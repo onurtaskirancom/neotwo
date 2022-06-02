202dc0b7d66e6b9fc8e57021fa54d5dc3b15e47e
onurtaskirancom <taskiranonur9@gmail.com>
2022-06-03 02:16:26 +0300

    Change google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,8 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import getConfig from 'next/config';
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import getConfig from "next/config";
 // import Prism from "prismjs";
 // import "prismjs/themes/prism-twilight.css"
 const { publicRuntimeConfig } = getConfig();
-
-
 
 class MyDocument extends Document {
   setGoogleTags() {
@@ -14,26 +12,37 @@ class MyDocument extends Document {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-0FD3WEHDCR');
-        `
+      
+        gtag('config', 'G-YBKTZ7HXSY');
+        `,
       };
     }
   }
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang='en'>
         <Head>
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link
-            rel="stylesheet preload prefetch"
-            as="style"
-            href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
+          <meta charSet='UTF-8' />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1.0'
           />
-          <link rel="stylesheet preload prefetch" href="/static/css/styles.css" as="style"/>
-          <link rel="shortcut icon" href="#" />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-0FD3WEHDCR"></script>
+          <link
+            rel='stylesheet preload prefetch'
+            as='style'
+            href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css'
+          />
+          <link
+            rel='stylesheet preload prefetch'
+            href='/static/css/styles.css'
+            as='style'
+          />
+          <link rel='shortcut icon' href='#' />
+          <script
+            async
+            src='https://www.googletagmanager.com/gtag/js?id=G-YBKTZ7HXSY'
+          ></script>
           <script dangerouslySetInnerHTML={this.setGoogleTags()} />
         </Head>
         <body>
@@ -41,8 +50,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
